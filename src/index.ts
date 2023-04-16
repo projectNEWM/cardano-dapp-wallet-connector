305cf09e89a606edbbe4790f1a8a36027459693e
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2023-04-15 19:45:47 -0700

    updates version

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as useConnectWallet } from "./hooks/useConnectWallet";
+export { default as ConnectWallet } from "./components/ConnectWallet";
+export { default as WalletButton } from "./components/WalletButton";
+export { default as WalletModal } from "./components/WalletModal";
+export * from "./utils";
