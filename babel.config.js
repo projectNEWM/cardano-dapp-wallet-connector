b981d29ca8fdddd60204401e46234f8e6393cb68
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2023-04-27 20:07:10 -0700

    implements babel-plugin-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   presets: ["@babel/preset-typescript", "@babel/preset-react", "@babel/preset-env"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        root: ["./src"],
+      },
+    ],
+  ],
 };
