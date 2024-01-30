f1211f277852eaba7b64af9c6911fa4fb48b46b1
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2024-01-30 12:04:38 -0800

    additional logic to remove nextjs warning

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -2,11 +2,19 @@ import { storageKey } from "common";
 import { State } from "./types";
 import { makeObservable } from "./utils";
 
+const getIsConnected = () => {
+  if (typeof localStorage !== "undefined") {
+    return !!localStorage.getItem(storageKey);
+  }
+
+  return false;
+};
+
 export const initialState: State = {
   isLoading: false,
   error: null,
   enabledWallet: null,
-  isConnected: !!window?.localStorage?.getItem(storageKey),
+  isConnected: getIsConnected(),
 };
 
 const store = makeObservable(initialState);
